26eca3e331d5b8b1705cd541e6d25759bad91209
Fan Chen <fan.chen@wisc.edu>
2020-08-04 23:40:01 -0500

    evaluate SNR
    - buffling/snr: evaluate the signal-to-noise ratio for bloom filters
    -examples/plot: plot analysis results

---
+++ buffling/snr.py
@@ -0,0 +1,106 @@
+# Copyright 2020 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
+"""Evaluate signal-to-noise ratio of the 'shuffling+flipping' algorithm."""
+
+import numpy as np
+import scipy as sp
+import pandas as pd
+
+# @title helpers
+def generate_transition_matrix(n_pub, p=0.1, first2=False, tol=1e-15):
+  '''Generate the transition matrix of logos. 
+
+  Args: 
+    n_pub: Integer, number of publishers.
+    p: Flipping probablity.
+    first2: Boolean, whether return only the first 2 rows.
+    tol: A small Real number. This will be add to the sum of each row, 
+      followed by a row normalizing. 
+  
+  Return: 
+    A numpy.array of n_pub x n_pub, containing the transition probability. 
+  '''
+  n_logo = 2**n_pub
+  n_row = 2 if first2 else n_logo
+  transition_mat = np.zeros((n_row, n_logo))
+  for i in range(n_row):
+    for j in range(n_logo):
+      n_p = bin(i ^ j)[2:].count('1')
+      transition_mat[i,j] = p**n_p * (1 - p)**(n_pub - n_p)
+  rs = transition_mat.sum(axis=1) + tol ## add a small number to row sums
+  return transition_mat / rs[:, np.newaxis]
+
+
+def calculate_snr_ij(i,j,input_logo_wgt,transition_mat,maximizer=False):
+  '''Calculate the SNR with fixed incremental change at i and j. 
+
+  This is a helper function to calculate_snr().
+
+  Args: 
+    i, j: Integer, indices of the two logo-counts that are changed. 
+    input_logo_wgt: Input logo-counts weights. 
+    transition_mat: Transition matrix of logo-counts with blipping. 
+    maximizer: Boolean, whether return the linear coefficients.
+
+  Return: 
+    A real number of SNR if maximizer is False. Otherwise, 
+    both the SNR and the maximizing logo-counts coefficients. 
+  '''
+  D = np.diag(input_logo_wgt.dot(transition_mat))
+  F = np.diag(input_logo_wgt)
+  B = D - (transition_mat.transpose()).dot(F.dot(transition_mat))
+  a = transition_mat[i,:] - transition_mat[j,:]
+  c = sp.linalg.solve(B, a)
+  c -= np.mean(c) 
+  c /= np.linalg.norm(c)
+  snr = sum((c * a) ** 2) / np.dot(c.transpose(), B.dot(c))
+  if maximizer: 
+    return snr, c
+  return snr
+
+def calculate_snr(input_logo_wgt,transition_mat):
+  """Calculate the SNR.
+
+  Args: 
+    input_logo_wgt: Input logo-counts weights. 
+    transition_mat: Transition matrix of logo-counts with blipping. 
+
+  Return: 
+    A real number, the maximal SNR.
+  """
+  n_logo = transition_mat.shape[0]
+  s = np.zeros(transition_mat.shape)
+  for i in range(n_logo):
+    for j in range(n_logo):
+      if i == j:
+        continue
+      s[i,j] = calculate_snr_ij(i,j,input_logo_wgt,transition_mat) 
+  return s
+
+def calculate_worst_snr(input_logo_wgt,transition_mat):
+  """Calculate the worst SNR (quickly).
+
+  Args: 
+    input_logo_wgt: Input logo-counts weights. 
+    transition_mat: Transition matrix of logo-counts with blipping. 
+
+  Return: 
+    A real number, the maximal SNR.
+  """
+  n_logo = transition_mat.shape[0]
+  i = 0
+  j = n_logo - 1
+  return calculate_snr_ij(i,j,input_logo_wgt,transition_mat) 
---
+++ examples/plot.py
@@ -0,0 +1,62 @@
+
+from buffling import snr
+import numpy as np
+import pandas as pd
+import seaborn as sns 
+import matplotlib.pyplot as plt
+
+## global parameters
+NUM_PUBS = 4
+FLIP_PROB = 0.05
+# plt.style.use("ggplot")
+
+## dependent parameters
+NUM_LOGO = 2 ** NUM_PUBS
+TRANSIT_MAT = snr.generate_transition_matrix(NUM_PUBS, FLIP_PROB)
+
+
+## heatmap: SNR vs (i, j)
+fig, ax = plt.subplots(1,4,figsize=(16, 3))
+for i in range(4): 
+  skewness = 3 * i 
+  input_logo_wgt = np.exp(- skewness * (np.arange(NUM_LOGO) + 1) / NUM_LOGO)
+  input_logo_wgt /= sum(input_logo_wgt)
+  s = snr.calculate_snr(input_logo_wgt, TRANSIT_MAT)
+  sns.heatmap(np.sqrt(s), ax = ax[i])
+  ax[i].set_title(f"SNR (skewness: {skewness})")
+plt.savefig('docs/fig/1_snr_vs_ij.pdf')
+plt.close()
+
+
+## line plot: maximizing coefficients vs skewness
+data = pd.DataFrame(columns=['skewness', 'logo', 'coefficient'])
+data = data.astype({'logo': 'category'})
+for skewness in (np.arange(6) * 2):
+  input_logo_wgt = np.exp(- skewness * (np.arange(NUM_LOGO) + 1) / NUM_LOGO)
+  input_logo_wgt /= sum(input_logo_wgt)
+  s,c = snr.calculate_snr_ij(0, NUM_LOGO-1, input_logo_wgt, TRANSIT_MAT, maximizer=True)
+  for i in range(len(c)):
+    data.loc[len(data)] = [skewness, i, c[i]]
+plt.figure(figsize=(6,4))
+sns.scatterplot(x="logo", y="coefficient", hue='skewness', data=data)
+sns.lineplot(x="logo", y="coefficient", hue='skewness', legend=False, data=data)
+plt.hlines(0, -.5, NUM_LOGO-1, colors="grey", linestyles="dashed")
+plt.title(f"logo-counts coefficients of SNR maximizer ({NUM_PUBS} pubs)")
+plt.savefig('docs/fig/2_max_logo_coefs.pdf')
+plt.close()
+
+
+## line plot: SNR vs skewness
+skewness_breaks = np.arange(8 * NUM_PUBS ** 2, step=NUM_PUBS ** 2 / 2)
+data = pd.DataFrame(columns=['skewness', 'SNR'])
+for skewness in skewness_breaks:
+  input_logo_wgt = np.exp(- skewness * (np.arange(NUM_LOGO) + 1) / NUM_LOGO)
+  input_logo_wgt /= sum(input_logo_wgt)
+  s = snr.calculate_worst_snr(np.array(input_logo_wgt), TRANSIT_MAT)
+  data.loc[len(data)] = [skewness, s]
+plt.figure(figsize=(6,4))
+sns.scatterplot(x="skewness", y="SNR", data=data)
+sns.lineplot(x="skewness", y="SNR", data=data)
+plt.title(f"signal-to-noise ratio ({NUM_PUBS} pubs)")
+plt.savefig('docs/fig/3_snr_vs_skewness.pdf')
+plt.close()
